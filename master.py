4c5fc23d87a5291d906a581f506eda8eb4dba5f3
gethappy <gethappy90@gmail.com>
2014-03-01 23:41:54 +0400

    Изменила уровень безопасности, setMainReplica() теперь запускается в самом начале. Пофиксила параметры запуска мастера и реплик

--- master.py
+++ master.py
@@ -3,22 +3,24 @@ import SocketServer
 import urllib2
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 from datetime import datetime
-#from time import time, sleep#gmtime, strftime
 import time
-
 import threading
 
-#MASTER_ADRESS = "127.10.0.1"
+
+if sys.argv[1:]:
+    port = int(sys.argv[1])
+else:
+    port = 8000
+    
+MASTER_ADRESS = "127.0.0.1" 
+
+server_adress =  (MASTER_ADRESS, port)
+MASTER_ADRESS = MASTER_ADRESS  + ':' + str(port)  
+
 REPLICS = ["127.0.0.1:8001", "127.0.0.1:8002", "127.0.0.1:8003"]
 MAIN_REPLICA = None
 
 class MasterHandler(SimpleHTTPRequestHandler):
-
- #   def __init__(self, *args):
- #       #Start some timer(handler = setMainReplica()) 
- #       #MasterHandler.setMainReplica()
- #       SimpleHTTPRequestHandler.__init__(self, *args)
-
     def do_GET(self):
         try:
             if self.path == "/get-write-replica":
@@ -36,16 +38,17 @@ class MasterHandler(SimpleHTTPRequestHandler):
     def getMainReplica():
         global MAIN_REPLICA
         return 'Not ready yet' if MAIN_REPLICA is None else MAIN_REPLICA
-        
-    @staticmethod                    
-    def setMainReplica():
+
+
+def setMainReplica():
         global REPLICS
-        global MAIN_REPLICA
+        global MAIN_REPLICA      
         #check current MAIN_REPLICA
         #return if answer
         if MAIN_REPLICA is not None:
             try:
                 req = urllib2.Request("http://" + MAIN_REPLICA + "/set-new-ticket")
+                req.add_header("referer", MASTER_ADRESS)
                 response = urllib2.urlopen(req)
                 if response.getcode() == 200:
                     return
@@ -57,6 +60,8 @@ class MasterHandler(SimpleHTTPRequestHandler):
         for replic_adress in REPLICS:
             try:
                 req = urllib2.Request("http://" + replic_adress + "/set-new-ticket")
+                req.add_header("referer", MASTER_ADRESS)
+                
                 response = urllib2.urlopen(req)
                 if response.getcode() == 200:
                     MAIN_REPLICA = replic_adress
@@ -65,22 +70,18 @@ class MasterHandler(SimpleHTTPRequestHandler):
                 pass
         MAIN_REPLICA = None
 
+
 def clock():
     while True:
-        MasterHandler.setMainReplica()
+        setMainReplica()
         time.sleep(50)
-    
-if sys.argv[1:]:
-    port = int(sys.argv[1])
-else:
-    port = 8000
-    
-server_address = ('127.0.0.1', port)
-httpd = SocketServer.TCPServer(("", port), MasterHandler)
+
+httpd = SocketServer.TCPServer(server_adress, MasterHandler)
 print "Master serving HTTP on port", port
 
 t = threading.Thread(target=clock)
 t.daemon = True
 t.start()
+setMainReplica()
 
 httpd.serve_forever()
--- replica.py
+++ replica.py
@@ -3,15 +3,30 @@ import SocketServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 from datetime import datetime, timedelta
 
-TICKET = 0
+
+
+
+if sys.argv[1:]:
+    port = int(sys.argv[1])
+else:
+    port = 8001
+
+if sys.argv[2:]:
+    MASTER_ADRESS = sys.argv[2]
+else:
+    MASTER_ADRESS = "127.0.0.1:8000"
+
+server_address = ('127.0.0.1', port)
+
 TICKET_LIVE_TIME = timedelta(seconds = 55)
-MASTER_ADRESS = "127.10.0.1:8000"
+TICKET = datetime.now() - TICKET_LIVE_TIME 
 
 class ReplicaHandler(SimpleHTTPRequestHandler):
 
     def do_GET(self):
         try:
             global TICKET
+            global MASTER_ADRESS
             if self.path == "/write":
                 answer = "KO" if TICKET < datetime.now() else "OK"
                 self.send_response(200)
@@ -22,33 +37,23 @@ class ReplicaHandler(SimpleHTTPRequestHandler):
                         #        '<br />now : ' + datetime.now().strftime("%d/%m/%y %H:%M")) 
                 #print 'GET : ' + TICKET.strftime("%d/%m/%y %H:%M")
             elif self.path == "/set-new-ticket":    
-                TICKET = datetime.now() + TICKET_LIVE_TIME
-                self.send_response(200)
-                self.send_header("Content-type", "text/html")
-                self.end_headers()
+                referer_url = self.headers.getheader('referer')
+                print referer_url
+                print MASTER_ADRESS
+                if referer_url == MASTER_ADRESS:
+                    TICKET = datetime.now() + TICKET_LIVE_TIME
+                    self.send_response(200)
+                    self.send_header("Content-type", "text/html")
+                    self.end_headers()
+                else:
+                    self.send_error(404,"File Not Found: %s" % self.path)
             else:                 
                 SimpleHTTPRequestHandler.do_GET(self)
         except IOError:
             self.send_error(404,"File Not Found: %s" % self.path)
      
- #   def do_POST(self):
- #       try:
- #           global TICKET
- #           if self.path == "/set-new-ticket":
- #               content_len = int(self.headers.getheader('content-length'))
- #               post_str = self.rfile.read(content_len)
- #               TICKET = datetime.strptime(post_str, "%d/%m/%y %H:%M")      
- #               #print 'POST : ' + TICKET.strftime("%d/%m/%y %H:%M")
- #       except Exception:
- #           self.send_error(404,"File Not Found: %s" % self.path)
 
 
-if sys.argv[1:]:
-    port = int(sys.argv[1])
-else:
-    port = 8001
-server_address = ('127.10.0.1', port)
-
-httpd = SocketServer.TCPServer(("", port), ReplicaHandler)
+httpd = SocketServer.TCPServer(server_address, ReplicaHandler)
 print "Replica serving HTTP on port", port
 httpd.serve_forever()
